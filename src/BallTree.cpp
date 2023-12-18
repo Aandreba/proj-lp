0af75ba4369b3bb01e7df9a4c7ba7e3fc7a78157
Aandreba <aandrebafreelancer@gmail.com>
2023-12-18 19:15:15 +0100

    vv

--- src/BallTree.cpp
+++ src/BallTree.cpp
@@ -6,66 +6,64 @@
 #include "MyUtils.hpp"
 #include "Util.h"
 
-class SortedCoordinate {
-   public:
-    Coordinate coord;
-    double dist;
-
-    bool operator<(const SortedCoordinate& other) const {
-        return dist < other.dist;
-    };
-
-    bool operator==(const SortedCoordinate& other) const {
-        return coord.lat == other.coord.lat && coord.lon == other.coord.lon;
-    };
-};
-
-void construirArbreRec(BallTree* self, std::vector<SortedCoordinate>::const_iterator start, std::vector<SortedCoordinate>::const_iterator end) {
-    const size_t size = std::distance(start, end);
-
-    for (auto coord = start; coord != end; coord++) {
-        self->getCoordenades().push_back(coord->coord);
-    }
+void construirArbreRec(BallTree* self, const std::vector<Coordinate>& coords) {
+    const size_t size = coords.size();
+    self->getCoordenades() = coords;
     if (size <= 1) return;
 
     // Calculate pivot/centroid
     Coordinate mean{0., 0.};
-    for (auto coord = start; coord != end; coord++) {
-        mean.lat += coord->coord.lat;
-        mean.lon += coord->coord.lon;
+    for (auto coord : coords) {
+        mean.lat += coord.lat;
+        mean.lon += coord.lon;
     }
     self->setPivot(Coordinate{mean.lat / (double)size, mean.lon / (double)size});
 
-    // Calculate mean distance
-    std::vector<SortedCoordinate> distances;
-    for (auto coord = start; coord != end; coord++) {
-        const double dist = Util::DistanciaHaversine(coord->coord, self->getPivot());
-        distances.push_back(SortedCoordinate{coord->coord, dist});
-    }
-    std::sort(distances.begin(), distances.end());
+    // Calculate distance from mean
+    Coordinate alpha;
+    double max_dist = NEG_INF;
 
-    // Split
-    self->setRadius(std::next(distances.cend(), -1)->dist);
-    const auto midpoint = std::next(distances.cbegin(), distances.size() / 2);
-
-    self->setEsquerre(new BallTree());
-    construirArbreRec(self->getEsquerre(), distances.cbegin(), midpoint);
-
-    self->setDreta(new BallTree());
-    construirArbreRec(self->getDreta(), midpoint, distances.cend());
-}
-
-void BallTree::construirArbre(const std::vector<Coordinate>& coordenades) {
-    std::vector<SortedCoordinate> coordinates;
-
-    // Remove equal coordinates
-    for (auto coord : coordenades) {
-        if (std::find(coordinates.begin(), coordinates.end(), SortedCoordinate{coord, INF}) == coordinates.end()) {
-            coordinates.push_back(SortedCoordinate{coord, INF});
+    for (auto coord : coords) {
+        const double dist = Util::DistanciaHaversine(coord, self->getPivot());
+        if (dist > max_dist) {
+            alpha = coord;
+            max_dist = dist;
         }
     }
 
-    construirArbreRec(this, coordinates.cbegin(), coordinates.cend());
+    // Calculate distance from max
+    Coordinate beta;
+    max_dist = NEG_INF;
+
+    for (auto coord : coords) {
+        const double dist = Util::DistanciaHaversine(coord, alpha);
+        if (dist > max_dist) {
+            beta = coord;
+            max_dist = dist;
+        }
+    }
+
+    std::vector<Coordinate> left;
+    std::vector<Coordinate> right;
+
+    for (auto coord : coords) {
+        const double d1 = Util::DistanciaHaversine(coord, alpha);
+        const double d2 = Util::DistanciaHaversine(coord, beta);
+
+        if (d1 < d2)
+            left.push_back(coord);
+        else
+            right.push_back(coord);
+    }
+
+    self->setEsquerre(new BallTree());
+    construirArbreRec(self->getEsquerre(), left);
+    self->setDreta(new BallTree());
+    construirArbreRec(self->getDreta(), right);
+}
+
+void BallTree::construirArbre(const std::vector<Coordinate>& coordenades) {
+    construirArbreRec(this, coordenades);
 }
 
 void BallTree::inOrdre(std::vector<std::list<Coordinate>>& out) {
